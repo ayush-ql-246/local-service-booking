c6769c2ef7c735e55e2d53059a73ccf41237ebf6
ayush-ql-246 <ayush.joshi@quokkalabs.com>
2025-09-23 17:38:24 +0530

    initial commit

---
+++ src/main/java/com/example/local_service_booking/LocalServiceBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_service_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalServiceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalServiceBookingApplication.class, args);
+	}
+
+}
